c104ff24415ebd2bac92d761ff13671ee8fd798f
David Gundry <davidgundry@gmail.com>
2016-04-08 10:43:17 +0100

    Togglable MT logging

--- Assets/MicrophoneTools/scripts/Log.cs
+++ Assets/MicrophoneTools/scripts/Log.cs
@@ -20,48 +20,50 @@ namespace MicTools
 
     public class LogMT
     {
+        private const bool mtLoggingOn = false;
+
         public static void Log(string text)
         {
-            //if (Debug.isDebugBuild)
-            //   Debug.Log(text);
-            TelemetryTools.Telemetry.Instance.SendEvent("Log: " + text);
+            if (mtLoggingOn)
+                TelemetryTools.Telemetry.Instance.SendEvent("Log: " + text);
         }
         public static void LogWarning(string text)
         {
-            //if (Debug.isDebugBuild)
-            //     Debug.LogWarning(text);
-            TelemetryTools.Telemetry.Instance.SendEvent("LogWarning: " + text);
+            if (mtLoggingOn)
+                TelemetryTools.Telemetry.Instance.SendEvent("LogWarning: " + text);
         }
         public static void LogError(string text)
         {
-            //if (Debug.isDebugBuild)
-            //     Debug.LogError(text);
-            TelemetryTools.Telemetry.Instance.SendEvent("LogError: " + text);
+            if (mtLoggingOn)
+                TelemetryTools.Telemetry.Instance.SendEvent("LogError: " + text);
         }
         public static void LogError(string text, Object o)
         {
-            //if (Debug.isDebugBuild)
-            //    Debug.LogError(text,o);
-            TelemetryTools.Telemetry.Instance.SendEvent("LogError: " + text);
+            if (mtLoggingOn)
+                TelemetryTools.Telemetry.Instance.SendEvent("LogError: " + text);
         }
         public static void SendStreamValue(string tag, System.ValueType value)
         {
-            TelemetryTools.Telemetry.Instance.SendStreamValue(tag, value);
+            if (mtLoggingOn)
+                TelemetryTools.Telemetry.Instance.SendStreamValue(tag, value);
         }
 
         public static void SendByteDataBase64(string tag, byte[] data)
         {
-            TelemetryTools.Telemetry.Instance.SendByteDataBase64(tag, data);
+            if (mtLoggingOn)
+                TelemetryTools.Telemetry.Instance.SendByteDataBase64(tag, data);
         }
 
         public static void SendStreamValueBlock(string tag, float[] data)
         {
-            TelemetryTools.Telemetry.Instance.SendStreamValueBlock(tag, data);
+            if (mtLoggingOn)
+                TelemetryTools.Telemetry.Instance.SendStreamValueBlock(tag, data);
         }
 
         public static void LogSoundEvent(SoundEvent e)
         {
-            TelemetryTools.Telemetry.Instance.SendEvent("SoundEvent:" + SoundEventToString(e));
+            if (mtLoggingOn)
+                TelemetryTools.Telemetry.Instance.SendEvent("SoundEvent:" + SoundEventToString(e));
         }
 
         private static string SoundEventToString(SoundEvent e)
