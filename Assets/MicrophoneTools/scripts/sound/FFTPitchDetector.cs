c1a79acc608266a045d4398473203e9216f6d39f
David Gundry <davidgundry@gmail.com>
2016-04-13 14:57:36 +0100

    Tidy code

--- Assets/MicrophoneTools/scripts/sound/FFTPitchDetector.cs
+++ Assets/MicrophoneTools/scripts/sound/FFTPitchDetector.cs
@@ -7,261 +7,261 @@ using System;
 
 namespace MicTools
 {
-    /* Much of the interacting with FFT code has been adapted from http://forum.unity3d.com/threads/fft-how-to.253192/*/
+/* Much of the interacting with FFT code has been adapted from http://forum.unity3d.com/threads/fft-how-to.253192/*/
 
-    [RequireComponent(typeof(MicrophoneController))]
-    [AddComponentMenu("MicrophoneTools/FormantFinder")]
-    public class FFTPitchDetector : MonoBehaviour
+[RequireComponent(typeof(MicrophoneController))]
+[AddComponentMenu("MicrophoneTools/FormantFinder")]
+public class FFTPitchDetector : MonoBehaviour
+{
+    private MicrophoneController microphoneController;
+
+    private float[] spectrum;
+    public float[] Spectrum
     {
-        private MicrophoneController microphoneController;
-
-        private float[] spectrum;
-        public float[] Spectrum
+        get
         {
-            get
-            {
-                return spectrum;
-            }
+            return spectrum;
         }
-        /*private const int sampleWindow = 128;
-        private static int spectrumSize = 8192;
+    }
+    /*private const int sampleWindow = 128;
+    private static int spectrumSize = 8192;
 
-        private const int activationThreshold = 1;
+    private const int activationThreshold = 1;
 
-        private int windowsSoFar;
-        public float noiseLevel = 1f;
-        public float NoiseLevel
+    private int windowsSoFar;
+    public float noiseLevel = 1f;
+    public float NoiseLevel
+    {
+        get
         {
-            get
-            {
-                return noiseLevel;
-            }
-        }*/
-
-        public float f0;
-
-        /*private FormantRecord[] formants;
-        public FormantRecord[] Formants
-        {
-            get
-            {
-                return formants;
-            }
+            return noiseLevel;
         }
+    }*/
 
-        public int f1;
-        public int f2;
-        public float f1freq;
-        public float f2freq;*/
+    public float f0;
 
-        FFT2 fft;
-        private const int windowSize = 1024*4;
-
-        public int channel = 0; // the channel to analyze
-
-        public int OutputLength
+    /*private FormantRecord[] formants;
+    public FormantRecord[] Formants
+    {
+        get
         {
-            get
-            {
-                if (_doFFT == false)
-                    return 0;
-
-                return windowSize / 2;
-            }
+            return formants;
         }
+    }
 
-        private float[] arrRe;
-        private float[] arrI;
-        private float[] window;
-        private bool _doFFT; // set to true when all is initialised. That way, no need for an expensive try catch.
+    public int f1;
+    public int f2;
+    public float f1freq;
+    public float f2freq;*/
 
-        // Init
-        void Awake()
-        {
-            microphoneController = GetComponent<MicrophoneController>();
+    FFT2 fft;
+    private const int windowSize = 1024*4;
 
-            arrRe = new float[windowSize];
-            arrI = new float[windowSize];
-            window = new float[windowSize];
-            window = Hanning(window);
-            spectrum = new float[windowSize / 2];
+    public int channel = 0; // the channel to analyze
 
-            fft = new FFT2();
-            uint logN = (uint)Math.Log(windowSize, 2);
-            fft.init(logN);
-
-            _doFFT = true;
-        }
-
-
-        void DoFFT(float[] data, int channels)
+    public int OutputLength
+    {
+        get
         {
             if (_doFFT == false)
-                return;
+                return 0;
 
-            // Step 1 : de-interleave
-            int j = 0;
-            for (int i = channel; i < data.Length; i += channels)
-            {
-                arrRe[j] = data[i];
-                j++;
-            }
+            return windowSize / 2;
+        }
+    }
 
-            // Apply precalculated windowing
-            for (int i = 0; i < arrRe.Length; i++)
-                arrRe[i] *= window[i];
+    private float[] arrRe;
+    private float[] arrI;
+    private float[] window;
+    private bool _doFFT; // set to true when all is initialised. That way, no need for an expensive try catch.
 
-            System.Array.Clear(arrI, 0, windowSize);
+    // Init
+    void Awake()
+    {
+        microphoneController = GetComponent<MicrophoneController>();
 
-            fft.run(arrRe, arrI);
+        arrRe = new float[windowSize];
+        arrI = new float[windowSize];
+        window = new float[windowSize];
+        window = Hanning(window);
+        spectrum = new float[windowSize / 2];
 
-            // Compute magnitude, in place
-            // Only compute half, as other half is useless
-            for (int i = 0; i < windowSize / 2; i++)
-                arrRe[i] = Mathf.Sqrt(arrRe[i] * arrRe[i] + arrI[i] * arrI[i]);
+        fft = new FFT2();
+        uint logN = (uint)Math.Log(windowSize, 2);
+        fft.init(logN);
 
-            System.Array.Copy(arrRe, spectrum, windowSize / 2);
+        _doFFT = true;
+    }
+
+
+    void DoFFT(float[] data, int channels)
+    {
+        if (_doFFT == false)
+            return;
+
+        // Step 1 : de-interleave
+        int j = 0;
+        for (int i = channel; i < data.Length; i += channels)
+        {
+            arrRe[j] = data[i];
+            j++;
         }
 
-        public void GetSpectrumDataSynched(float[] data)
+        // Apply precalculated windowing
+        for (int i = 0; i < arrRe.Length; i++)
+            arrRe[i] *= window[i];
+
+        System.Array.Clear(arrI, 0, windowSize);
+
+        fft.run(arrRe, arrI);
+
+        // Compute magnitude, in place
+        // Only compute half, as other half is useless
+        for (int i = 0; i < windowSize / 2; i++)
+            arrRe[i] = Mathf.Sqrt(arrRe[i] * arrRe[i] + arrI[i] * arrI[i]);
+
+        System.Array.Copy(arrRe, spectrum, windowSize / 2);
+    }
+
+    public void GetSpectrumDataSynched(float[] data)
+    {
+        if (data.Length < windowSize / 2)
         {
-            if (data.Length < windowSize / 2)
-            {
-                Debug.LogWarning("Provided array length should be at least " + (windowSize / 2).ToString());
-                return;
-            }
-            System.Array.Copy(spectrum, data, windowSize / 2); //No need to copy the mirrored part
+            Debug.LogWarning("Provided array length should be at least " + (windowSize / 2).ToString());
+            return;
         }
+        System.Array.Copy(spectrum, data, windowSize / 2); //No need to copy the mirrored part
+    }
 
-        private float[] Hanning(float[] input)
+    private float[] Hanning(float[] input)
+    {
+        for (int i = 0; i < input.Length; i++)
         {
-            for (int i = 0; i < input.Length; i++)
-            {
-                input[i] = (0.5f * (1.0f - Mathf.Cos((2.0f * Mathf.PI * i) / windowSize)));
-            }
-            return input;
+            input[i] = (0.5f * (1.0f - Mathf.Cos((2.0f * Mathf.PI * i) / windowSize)));
         }
+        return input;
+    }
 
-        void Update()
+    void Update()
+    {
+        float[] window = microphoneController.GetMostRecentSamples(windowSize);
+        DoFFT(window, 1);//microphoneBuffer.Channels);
+        //windowsSoFar++;
+
+        f0 = IndexToFrequency(HighestPoint(spectrum));
+
+
+        /*float mean = 0; MicrophoneInput.SumIntensity(spectrum) / spectrum.Length;
+        noiseLevel += (mean - noiseLevel) / windowsSoFar;
+        formants = PeakPicking(spectrum, 0);
+
+        if (formants.Length > 1)
         {
-            float[] window = microphoneController.GetMostRecentSamples(windowSize);
-            DoFFT(window, 1);//microphoneBuffer.Channels);
-            //windowsSoFar++;
+            f1 = formants[1].Peak;
+            if (formants.Length > 2)
+                f2 = formants[2].Peak;
+            else
+                f2 = 0;
+        }
+        else
+        {
+            f1 = 0;
+            f2 = 0;
+        }
+        f1freq = IndexToFrequency(f1);
+        f2freq = IndexToFrequency(f2);*/
 
-            f0 = IndexToFrequency(HighestPoint(spectrum));
+    }
 
+    /*private FormantRecord[] PeakPicking(float[] data, float noiseLevel)
+    {
+        List<FormantRecord> formants = new List<FormantRecord>();
 
-            /*float mean = 0; MicrophoneInput.SumIntensity(spectrum) / spectrum.Length;
-            noiseLevel += (mean - noiseLevel) / windowsSoFar;
-            formants = PeakPicking(spectrum, 0);
+        int start = 0;
+        int end = 0;
+        int peak = 0;
+        float highest = 0;
 
-            if (formants.Length > 1)
+        bool inPeak = false;
+
+        for (int i = 5; i < data.Length; i++)
+        {
+            if (!inPeak)
             {
-                f1 = formants[1].Peak;
-                if (formants.Length > 2)
-                    f2 = formants[2].Peak;
-                else
-                    f2 = 0;
+                if (data[i] > noiseLevel * activationThreshold)
+                {
+                    start = i;
+                    inPeak = true;
+                }
             }
             else
             {
-                f1 = 0;
-                f2 = 0;
-            }
-            f1freq = IndexToFrequency(f1);
-            f2freq = IndexToFrequency(f2);*/
-
-        }
-
-        /*private FormantRecord[] PeakPicking(float[] data, float noiseLevel)
-        {
-            List<FormantRecord> formants = new List<FormantRecord>();
-
-            int start = 0;
-            int end = 0;
-            int peak = 0;
-            float highest = 0;
-
-            bool inPeak = false;
-
-            for (int i = 5; i < data.Length; i++)
-            {
-                if (!inPeak)
-                {
-                    if (data[i] > noiseLevel * activationThreshold)
-                    {
-                        start = i;
-                        inPeak = true;
-                    }
-                }
-                else
-                {
-                    if (data[i] > highest)
-                    {
-                        highest = data[i];
-                        peak = i;
-                    }
-                    if (data[i] < noiseLevel * activationThreshold)
-                    {
-                        end = i;
-                        inPeak = false;
-                        formants.Add(new FormantRecord(start, end, peak));
-                        start = 0;
-                        end = 0;
-                        peak = 0;
-                        highest = 0;
-                    }
-                }
-            }
-
-            return formants.ToArray();
-        }*/
-
-        private static int HighestPoint(float[] data)
-        {
-            int index = 0;
-            float highest = 0;
-            for (int i = 0; i < data.Length; i++)
-            {
                 if (data[i] > highest)
                 {
-                    index = i;
                     highest = data[i];
+                    peak = i;
+                }
+                if (data[i] < noiseLevel * activationThreshold)
+                {
+                    end = i;
+                    inPeak = false;
+                    formants.Add(new FormantRecord(start, end, peak));
+                    start = 0;
+                    end = 0;
+                    peak = 0;
+                    highest = 0;
                 }
             }
-            return index;
         }
 
-        public static float IndexToFrequency(int index)
-        {
-            return index * 44100 / windowSize;
-        }
+        return formants.ToArray();
+    }*/
 
-        private static float FrequencyLevel(float[] spectrum, float frequency)
+    private static int HighestPoint(float[] data)
+    {
+        int index = 0;
+        float highest = 0;
+        for (int i = 0; i < data.Length; i++)
         {
-            int index = (int)Mathf.Round(((1 / 48000) * windowSize * 2) * frequency);
-            return spectrum[index];
-        }
-
-        private static int FrequencyToIndex(float frequency)
-        {
-            return (int)Mathf.Round(((1 / 48000) * windowSize * 2) * frequency);
-        }
-
-        private static float SumSpectrumArea(float[] spectrum, float low, float high)
-        {
-            int min = FrequencyToIndex(low);
-            int max = spectrum.Length - 1;
-            if (high != 0)
-                max = FrequencyToIndex(high);
-
-            float sum = 0;
-            for (int i = min; i <= max; i++)
+            if (data[i] > highest)
             {
-                sum += spectrum[i];
+                index = i;
+                highest = data[i];
             }
-            return sum;
         }
-
+        return index;
     }
+
+    public static float IndexToFrequency(int index)
+    {
+        return index * 44100 / windowSize;
+    }
+
+    private static float FrequencyLevel(float[] spectrum, float frequency)
+    {
+        int index = (int)Mathf.Round(((1 / 48000) * windowSize * 2) * frequency);
+        return spectrum[index];
+    }
+
+    private static int FrequencyToIndex(float frequency)
+    {
+        return (int)Mathf.Round(((1 / 48000) * windowSize * 2) * frequency);
+    }
+
+    private static float SumSpectrumArea(float[] spectrum, float low, float high)
+    {
+        int min = FrequencyToIndex(low);
+        int max = spectrum.Length - 1;
+        if (high != 0)
+            max = FrequencyToIndex(high);
+
+        float sum = 0;
+        for (int i = min; i <= max; i++)
+        {
+            sum += spectrum[i];
+        }
+        return sum;
+    }
+
+}
 }
--- Assets/MicrophoneTools/scripts/sound/MicrophoneInputTest.cs
+++ Assets/MicrophoneTools/scripts/sound/MicrophoneInputTest.cs
@@ -5,34 +5,34 @@ using System.Collections;
 
 namespace MicTools
 {
-    [CustomEditor(typeof(MicTools.MicrophoneInput))]
-    public class MicrophoneInputTest : Editor
+[CustomEditor(typeof(MicTools.MicrophoneInput))]
+public class MicrophoneInputTest : Editor
+{
+    public AudioClip testClip;
+
+    protected MicrophoneInput microphoneInput;
+
+    void Awake()
     {
-        public AudioClip testClip;
-
-        protected MicrophoneInput microphoneInput;
-
-        void Awake()
-        {
-            microphoneInput = (MicrophoneInput)target;
-        }
-
-        public override void OnInspectorGUI()
-        {
-            DrawDefaultInspector();
-
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("Test");
-            testClip = (AudioClip)EditorGUILayout.ObjectField(testClip, typeof(AudioClip), false);
-            if (GUILayout.Button("Test"))
-            {
-                Debug.Log("Test Result: Syllables: " + MicrophoneInput.RunTest(testClip));
-            }
-            GUILayout.EndHorizontal();
-
-            Repaint();
-
-        }
+        microphoneInput = (MicrophoneInput)target;
     }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Test");
+        testClip = (AudioClip)EditorGUILayout.ObjectField(testClip, typeof(AudioClip), false);
+        if (GUILayout.Button("Test"))
+        {
+            Debug.Log("Test Result: Syllables: " + MicrophoneInput.RunTest(testClip));
+        }
+        GUILayout.EndHorizontal();
+
+        Repaint();
+
+    }
+}
 }
 #endif
